1f70bee3ed336a15b19851bfc7a54b7dc48fe64a
pierrototo <pierre69marion@gmail.com>
2016-10-28 13:22:41 +0200

    Edit getbeer

--- src/app/Beers/Components/beerGet.component.ts
+++ src/app/Beers/Components/beerGet.component.ts
@@ -4,33 +4,32 @@
 import { Component, OnInit,} from '@angular/core';
 import { Beer } from '../Model/beer';
 import { BeerService } from '../Services/beer.service';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: '../Template/beerList.component.html',
+  templateUrl: '../Template/beerGet.component.html',
   styleUrls: ['../../app.component.css']
 })
 
 export class BeerGetComponent implements OnInit{
 
   title : 'Liste de bieres ';
-  beers : Beer[];
+  beer : Beer;
 
-  constructor(private beerService: BeerService) {}
+  constructor(private beerService : BeerService,  private route: ActivatedRoute) {
+    this.route.params.subscribe(params => {
+      let id = params['id'];
+      this.beerService.getBeer(id).subscribe(beer => this.beer = beer);
+    });
+  }
 
   ngOnInit() {
     // Load comments
-    this.loadBeers()
-  }
-
-  loadBeers() {
-    // Get all comments
-    this.beerService.getBeers()
-      .subscribe(
-        beers => this.beers = beers, //Bind to view
-        err => {
-          // Log errors if any
-          console.log(err);
-        });
+    this.beerService.getBeer(this.beer.id).subscribe(
+      err => {
+        // Log errors if any
+        console.log(err);
+      });
   }
 }
