98804703d2de5aab6ba5d02c0dd478d113f5f824
Geslain <geslain.dahan@gmail.com>
2016-11-06 03:07:11 +0100

    Maps + style

--- src/app/Home/home.component.ts
+++ src/app/Home/home.component.ts
@@ -5,64 +5,114 @@ import { Component, NgModule, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { AgmCoreModule, MapsAPILoader } from 'angular2-google-maps/core';
+import {BarService} from "../Bars/Services/bar.service";
+import {BeerService} from "../Beers/Services/beer.service";
+import PlaceSearchRequest = google.maps.places.PlaceSearchRequest;
 
 @Component({
   selector: 'my-app',
-  styles: [`
-    .sebm-google-map-container {
-      height: 300px;
-    }
-  `],
-  template: `
-    <div class="container">
-      <h1>Angular 2 + Google Maps Places Autocomplete</h1>
-      <div class="form-group">
-        <input id="autocompleteInput" placeholder="search for location" autocorrect="off" autocapitalize="off" spellcheck="off" type="text" class="form-control" #search [formControl]="searchControl">
-      </div>
-      <sebm-google-map [latitude]="latitude" [longitude]="longitude" [scrollwheel]="false" [zoom]="zoom">
-        <sebm-google-map-marker [latitude]="latitude" [longitude]="longitude"></sebm-google-map-marker>
-      </sebm-google-map>
-    </div>
-  `
+  styleUrls: ["home.component.css"],
+  templateUrl: 'home.component.html',
 })
 export class HomeComponent implements OnInit {
 
-  public latitude: number;
-  public longitude: number;
+  public userLatitude= 45.750000;
+  public userLongitude = 4.850000;
   public searchControl: FormControl;
   public zoom: number;
 
   @ViewChild("search")
   public searchElementRef: any;
+  public beers;
+  public beersNew;
+  public bars;
+  public barsNew;
 
   constructor(
-    private mapsAPILoader: MapsAPILoader
-  ) {}
+    private mapsAPILoader: MapsAPILoader,
+    private barService : BarService,
+    private beerService : BeerService,
+  ) {
+    this.barService.getBars()
+      .subscribe(
+        bars => {
+          this.bars = bars;
+          this.barsNew = bars.slice(0,3);
+        }
+      );
+    this.beerService.getBeers()
+      .subscribe(
+        beers => {
+          this.beers = beers;
+          this.beersNew = beers.slice(0,3);
+        }
+      );
+  }
+
+  public changeBeer($event) {
+    location.replace("/beers/"+$event)
+  }
+
+  public changeBar($event) {
+    location.replace("/bars/"+$event)
+  }
 
   ngOnInit() {
-    //set google maps defaults
     this.zoom = 4;
-    this.latitude = 39.8282;
-    this.longitude = -98.5795;
 
     //create search FormControl
     this.searchControl = new FormControl();
+    let options = {
+      types: [
+        'establishment',
+      ],
+      componentRestrictions: {country: 'FR'}
+    };
+
 
     //set current position
     this.setCurrentPosition();
 
     //load Places Autocomplete
     this.mapsAPILoader.load().then(() => {
-      let autocomplete = new google.maps.places.Autocomplete(<HTMLInputElement>document.getElementById("autocompleteInput"), {
-
+      var userPosition = new google.maps.LatLng(this.userLatitude, this.userLongitude);
+      var map = new google.maps.Map(document.getElementById('map'), {
+        center:userPosition,
+        zoom: 15,
+        scrollwheel: true
       });
+
+      var request = {
+        location: userPosition,
+        radius: '1000',
+        types: ['bar']
+      };
+
+      var service = new google.maps.places.PlacesService(map);
+      service.nearbySearch(<PlaceSearchRequest>request, function(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            var place = results[i];
+            // If the request succeeds, draw the place location on
+            // the map as a marker, and register an event to handle a
+            // click on the marker.
+            var marker = new google.maps.Marker({
+              map: map,
+              position: place.geometry.location
+            });
+          }
+        }
+      });
+
+
+      let autocomplete = new google.maps.places.Autocomplete(<HTMLInputElement>document.getElementById("autocompleteInput"), options);
       autocomplete.addListener("place_changed", () => {
         //get the place result
         let place: google.maps.places.PlaceResult = autocomplete.getPlace();
 
         //set latitude and longitude
-        this.latitude = place.geometry.location.lat();
-        this.longitude = place.geometry.location.lng();
+        this.userLatitude = place.geometry.location.lat();
+        this.userLongitude = place.geometry.location.lng();
       });
     });
   }
@@ -70,8 +120,8 @@ export class HomeComponent implements OnInit {
   private setCurrentPosition() {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition((position) => {
-        this.latitude = position.coords.latitude;
-        this.longitude = position.coords.longitude;
+        this.userLatitude = position.coords.latitude;
+        this.userLongitude = position.coords.longitude;
         this.zoom = 12;
       });
     }
